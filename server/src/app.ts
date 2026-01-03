02f63979d52eea5308318e1bb75d0e770dd45da5
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2026-01-03 17:11:45 +0530

    feat: implements server - adds config dir - adds server and app

---
+++ server/src/app.ts
@@ -0,0 +1,44 @@
+import { createServer } from 'http';
+import express, { type Express, type Request, type Response } from 'express';
+import { Server } from 'socket.io';
+import { CLIENT_PORT, NODE_ENV, SECRET_COOKIE } from '#config/env.js';
+import cors from 'cors';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import routes from '#routes/index.routes.js';
+
+const app: Express = express();
+
+const server = createServer(app);
+const io = new Server();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// CORS
+app.use(
+  cors({
+    origin: [`http://localhost:${CLIENT_PORT}`],
+    credentials: true
+  })
+);
+
+app.use(helmet());
+app.use(cookieParser(SECRET_COOKIE));
+
+// App Routes
+app.use('/', routes);
+
+app.get('/health', (req: Request, res: Response) => {
+  res.json({
+    url: req.url,
+    status: 'OK',
+    timestamp: new Date().toLocaleTimeString(),
+    environment: NODE_ENV,
+    uptime: Math.floor(process.uptime()),
+  });
+});
+
+
+export { app, server, io };
---
+++ server/src/config/db.ts
@@ -0,0 +1,20 @@
+import { connect } from 'mongoose';
+import { DATABASE_NAME, DATABASE_URL } from '#config/env.js';
+
+export const connectDB = async (): Promise<void> => {
+  if (!DATABASE_URL || !DATABASE_NAME) {
+    console.log(
+      'Error: DATABASE_URL or DATABASE_NAME is not defined in environment variables.'
+    );
+    return;
+  }
+
+  try {
+    const connectionString = `${DATABASE_URL}/${DATABASE_NAME}`;
+
+    await connect(connectionString);
+    console.log('Success: 🔥 Database connected successfully');
+  } catch (error) {
+    console.log('ERROR: ', error);
+  }
+};
